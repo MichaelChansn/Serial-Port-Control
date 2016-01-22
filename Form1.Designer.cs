efbf14785be66de35bfe1af1d4ef432799c41d20
MichaelChansn <13672025181@163.com>
2016-01-22 16:17:54 +0800

    init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace 串口通信
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.comboBox5);
+            this.groupBox1.Controls.Add(this.comboBox4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            resources.GetString("comboBox5.Items"),
+            resources.GetString("comboBox5.Items1"),
+            resources.GetString("comboBox5.Items2"),
+            resources.GetString("comboBox5.Items3")});
+            resources.ApplyResources(this.comboBox5, "comboBox5");
+            this.comboBox5.Name = "comboBox5";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            resources.GetString("comboBox4.Items"),
+            resources.GetString("comboBox4.Items1"),
+            resources.GetString("comboBox4.Items2"),
+            resources.GetString("comboBox4.Items3")});
+            resources.ApplyResources(this.comboBox4, "comboBox4");
+            this.comboBox4.Name = "comboBox4";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            resources.GetString("comboBox2.Items"),
+            resources.GetString("comboBox2.Items1"),
+            resources.GetString("comboBox2.Items2")});
+            resources.ApplyResources(this.comboBox2, "comboBox2");
+            this.comboBox2.Name = "comboBox2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            resources.GetString("comboBox3.Items"),
+            resources.GetString("comboBox3.Items1"),
+            resources.GetString("comboBox3.Items2"),
+            resources.GetString("comboBox3.Items3"),
+            resources.GetString("comboBox3.Items4"),
+            resources.GetString("comboBox3.Items5"),
+            resources.GetString("comboBox3.Items6"),
+            resources.GetString("comboBox3.Items7"),
+            resources.GetString("comboBox3.Items8"),
+            resources.GetString("comboBox3.Items9"),
+            resources.GetString("comboBox3.Items10"),
+            resources.GetString("comboBox3.Items11"),
+            resources.GetString("comboBox3.Items12")});
+            resources.ApplyResources(this.comboBox3, "comboBox3");
+            this.comboBox3.Name = "comboBox3";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2"),
+            resources.GetString("comboBox1.Items3"),
+            resources.GetString("comboBox1.Items4"),
+            resources.GetString("comboBox1.Items5")});
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.Name = "comboBox1";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button7);
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.button3);
+            resources.ApplyResources(this.groupBox3, "groupBox3");
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.TabStop = false;
+            // 
+            // button7
+            // 
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.Name = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.Name = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsReturn = true;
+            this.textBox2.AcceptsTab = true;
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button10);
+            this.groupBox4.Controls.Add(this.button9);
+            this.groupBox4.Controls.Add(this.button8);
+            this.groupBox4.Controls.Add(this.textBox2);
+            resources.ApplyResources(this.groupBox4, "groupBox4");
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.TabStop = false;
+            // 
+            // button10
+            // 
+            resources.ApplyResources(this.button10, "button10");
+            this.button10.Name = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            resources.ApplyResources(this.button9, "button9");
+            this.button9.Name = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            resources.ApplyResources(this.button8, "button8");
+            this.button8.Name = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.CodeDom;
+
+
+namespace 串口通信
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private bool isopen = false;
+        //private bool got = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 4;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 0;
+            comboBox4.SelectedIndex = 1;
+            comboBox5.SelectedIndex = 0;
+            //textBox2.Text = "HELLO";
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "打开串口")
+            {
+                button2.Text = "关闭串口";
+                serialPort1.PortName = comboBox3.Text.Trim().ToString();
+                serialPort1.BaudRate = Convert.ToInt32(comboBox1.Text);
+                serialPort1.DataBits = Convert.ToInt32(comboBox5.Text);
+                if (comboBox4.Text == "0")
+                    serialPort1.StopBits = StopBits.None;
+                if (comboBox4.Text == "1")
+                    serialPort1.StopBits = StopBits.One;
+                if (comboBox4.Text == "1.5")
+                    serialPort1.StopBits = StopBits.OnePointFive;
+                if (comboBox4.Text == "2")
+                    serialPort1.StopBits = StopBits.Two;
+                if (comboBox2.Text == "无")
+                    serialPort1.Parity = Parity.None;
+                if (comboBox2.Text == "奇校验")
+                    serialPort1.Parity = Parity.Odd;
+                if (comboBox2.Text == "偶校验")
+                    serialPort1.Parity = Parity.Even;
+                serialPort1.ReadTimeout = 100;
+                try
+                {
+                    serialPort1.Open();
+                    
+                    isopen = true;
+                    textBox1.Clear();
+
+                    textBox1.Text += serialPort1.PortName + "打开成功\r\n" + "已打开串口信息：" + "\r\n" + "串口号：" + serialPort1.PortName + "\r\n" + "波特率：" + serialPort1.BaudRate.ToString() + "\r\n" + "数据位：" + serialPort1.DataBits.ToString() + "\r\n" + "停止位：" + serialPort1.StopBits.ToString() + "\r\n" + "校验位：" + serialPort1.Parity.ToString() + "\r\n";
+                }
+                catch
+                {
+                    textBox1.Clear();
+                    textBox1.Text += serialPort1.PortName + "打开失败" + "\r\n";
+                    textBox1.Text += "希望打开串口信息：" + "\r\n" + "串口号：" + serialPort1.PortName + "\r\n" + "波特率：" + serialPort1.BaudRate.ToString() + "\r\n" + "数据位：" + serialPort1.DataBits.ToString() + "\r\n" + "停止位：" + serialPort1.StopBits.ToString() + "\r\n" + "校验位：" + serialPort1.Parity.ToString() + "\r\n";
+
+
+                }
+            }
+            else
+            {
+                button2.Text = "打开串口";
+                serialPort1.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    serialPort1.PortName = "COM" + (i+1).ToString();
+                    serialPort1.Open();
+                    textBox1.Text += serialPort1.PortName + "可用"+"\r\n";
+                    serialPort1.Close();
+                    
+                }
+                catch
+                {
+                    textBox1.Text += "COM" + (i+1).ToString() + "不可用"+"\r\n";
+                    serialPort1.Close();
+                    
+ 
+                }
+ 
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Write("l");
+                textBox2.Clear();
+                textBox2.Text += "当前状态：向左";
+            }
+            catch
+            {
+                //MessageBox.Show("向左控制失败");
+                textBox2.Clear();
+                textBox2.Text += "向左控制失败";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Write("u");
+                textBox2.Clear();
+                textBox2.Text += "当前状态：向上";
+            }
+            catch
+            {
+               // MessageBox.Show("向上控制失败");
+                textBox2.Clear();
+                textBox2.Text += "向上控制失败";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Write("d");
+                textBox2.Clear();
+                textBox2.Text += "当前状态：向下";
+            }
+            catch
+            {
+              //  MessageBox.Show("向下控制失败");
+                textBox2.Clear();
+                textBox2.Text += "向下控制失败";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Write("r");
+                textBox2.Clear();
+                textBox2.Text += "当前状态：向右";
+            }
+            catch
+            {
+               // MessageBox.Show("向右控制失败");
+                textBox2.Clear();
+                textBox2.Text += "向右控制失败";
+            }
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Write("s");
+                textBox2.Clear();
+                textBox2.Text += "当前状态：停止";
+            }
+            catch
+            {
+                // MessageBox.Show("停止控制失败");
+                textBox2.Clear();
+                textBox2.Text += "停止控制失败";
+            }
+        }
+
+       
+
+        /*
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode ==System.Windows.Forms.Keys.Left)
+            {
+                button4.Focus();//=true; ;
+                button4_Click(null, null);
+                
+            }
+            if (e.KeyCode == System.Windows.Forms.Keys.Right)
+            {
+                button5.Focus();//=true; ;
+                button5_Click(null, null);
+
+            }
+            if (e.KeyCode == System.Windows.Forms.Keys.Up)
+            {
+                button3.Focus();//=true; ;
+                button3_Click(null, null);
+
+            }
+            if (e.KeyCode == System.Windows.Forms.Keys.Down)
+            {
+                button6.Focus();//=true; ;
+                button6_Click(null, null);
+
+            }
+        }
+        */
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == System.Windows.Forms.Keys.Left)
+            {
+                button4.Focus();//=true; ;
+                button4_Click(null, null);
+               // button4.Focus();
+
+            }
+            if (e.KeyCode == System.Windows.Forms.Keys.Right)
+            {
+                button5.Focus();//=true; ;
+                button5_Click(null, null);
+                //button5.Focus();
+            }
+            if (e.KeyCode == System.Windows.Forms.Keys.Up)
+            {
+                button3.Focus();//=true; ;
+                button3_Click(null, null);
+                //button3.Focus();
+            }
+            if (e.KeyCode == System.Windows.Forms.Keys.Down)
+            {
+                button6.Focus();//=true; ;
+                button6_Click(null, null);
+               // button6.Focus();
+             
+            }
+            if (e.KeyCode == System.Windows.Forms.Keys.Space)
+            { 
+                button7.Focus();//=true; ;
+                button7_Click(null, null);
+                //button7.Focus();
+
+            }
+        }
+
+        
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            serialPort1.WriteLine(textBox2.Text);
+            textBox2.Clear();
+            
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            //textBox2.Text = serialPort1.ReadExisting();// ReadLine() + "\r\n";
+                //if (button9.Text == "接收数据")
+                //{
+                //    button9.Text = "停止接收";
+                //    timer1.Enabled = true;
+                //    try
+                //    {
+                //        textBox2.Clear();
+                //        textBox2.Text += serialPort1.ReadLine() + "\r\n";
+                //    }
+                //    catch
+                //    {
+                //        timer1.Enabled = false;
+                        
+                //    }
+                //}
+                //else
+                //{
+                //    button9.Text = "接收数据";
+                //    timer1.Enabled = false;
+                //}
+        }
+        private string tempstr;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isopen == true)//&&got==true)
+            {
+                timer1.Enabled = false;
+                //got = false;
+                try
+                {
+                    tempstr = serialPort1.ReadExisting();//ReadExisting();//ReadLine() + "\r\n";
+                    textBox2.Text += tempstr.Substring(tempstr.Length - 4, 4);
+                }
+                catch
+                {
+                    timer1.Enabled = false;
+                }
+            }
+            
+            timer1.Enabled = true;
+           // got = true;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (button10.Text == "开始接收")
+            {
+                button10.Text = "关闭接收";
+                timer1.Enabled = true;
+            }
+            else
+            {
+
+                button10.Text = "开始接收";
+                timer1.Enabled = false;
+            }
+
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //textBox2.Text += serialPort1.ReadLine() + "\r\n";
+           // got = true;
+            
+        }
+
+        
+
+     
+        
+        
+
+        
+
+
+        
+    }
+}
